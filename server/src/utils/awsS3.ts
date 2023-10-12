27e3e25fb05c39fb9da5e9529ee32c23e2aea61f
Even Qian <evenwin88@gmail.com>
2023-10-12 14:07:32 +1100

    fix: AWS key

--- server/src/utils/awsS3.ts
+++ server/src/utils/awsS3.ts
@@ -1,19 +1,20 @@
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import sharp from "sharp";
+import { AWS_ACCESS_KEY_ID, AWS_S3_BUCKET_NAME, AWS_SECRET_ACCESS_KEY } from "../variables";
 
 const client = new S3Client({
   region: "ap-southeast-2",
   credentials: {
-    accessKeyId: "AKIAVRIUTPWZILZRRB7G",
-    secretAccessKey: "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",
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
   },
 });
 
 export const awsUploadImgPost = async (fileUpload: any) => {
   const fileBuffer = await sharp(fileUpload.path).toBuffer();
   const command = new PutObjectCommand({
-    Bucket: "node-instagram-upload-bucket",
+    Bucket: AWS_S3_BUCKET_NAME,
     Key: fileUpload.filename,
     Body: fileBuffer,
   });
@@ -23,7 +24,7 @@ export const awsUploadImgPost = async (fileUpload: any) => {
       const url = await getSignedUrl(
         client,
         new GetObjectCommand({
-          Bucket: "node-instagram-upload-bucket",
+          Bucket: AWS_S3_BUCKET_NAME,
           Key: fileUpload.filename,
         })
       );
